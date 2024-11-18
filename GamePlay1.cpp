6e738baf737bbee4f9145ea9d1bb911d1a6587db
ThaiDuong2772 <23520342@gm.uit.edu.vn>
2024-11-18 19:07:43 +0700

    gameplay1_raw

---
+++ GamePlay1.cpp
@@ -0,0 +1,34 @@
+void Gameplay1(int mucdo)
+{
+    int DIEM=0;
+    gotoxy(tuongtrai+24,tuongtren-2);
+    cout<<"DIEM CUA BAN: ";
+    gotoxy(tuongtrai+38,tuongtren-2);
+    cout<<DIEM;
+    CONRAN r;
+    QUA qua;
+    char t;
+    Ve_Tuong();
+    TaoQua(qua,r);
+    qua.Ve();
+  while (1){
+        r.XoaCu();
+        r.Ve();
+        if(Game_Over1(r))
+        {
+            gotoxy(tuongtrai+25,tuongduoi-10);
+            cout<<"BAN DA CHET";
+            break;
+        }
+        if (kbhit()){
+            t = getch();
+            if (t=='a'&& Huong!=0) Huong = 2;
+            if (t=='w'&& Huong!=1) Huong = 3;
+            if (t=='d'&& Huong!=2) Huong = 0;
+            if (t=='s'&& Huong!=3) Huong = 1;
+        }
+        AnQua(qua,r,DIEM);
+        r.DiChuyen(Huong);
+        Sleep(mucdo);
+    }
+}
